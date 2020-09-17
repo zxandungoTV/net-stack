64fe51ba378b183b688b4ee32f47116408c58e72
zxanderrodriguez <zxanderrodriguez@gmail.com>
2020-09-17 12:36:22 -0500

    seconf interview

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
 
 namespace net_stack_tests
 {
@@ -15,6 +16,57 @@ namespace net_stack_tests
     }
 
     // implement tests
+    //		isEmpty() returns true if Stack is empty	
+    [TestMethod]
+    public void isStackEmpty()
+        {
+            Assert.AreEqual(true, stack.isEmpty());
+        }
 
-  }
+   //isEmpty() returns false after calling push(value)
+   [TestMethod]
+   public void isEmpty_false_whenPushedIsCalled()
+        {
+            stack.push(10);
+            Assert.AreEqual(false, stack.isEmpty());
+        }
+
+   // peek() returns null if Stack is empty
+  [TestMethod]
+  public void peek_returns_null_when_stack_is_Empty()
+      {
+            //Assert.AreEqual(true, stack.isEmpty());
+            Assert.AreEqual(null, stack.peek());
+      }
+        //peek() returns most recent pushed value
+  [TestMethod]
+   public void peek_returns_most_recent_value_whenNotEmpty()
+    {
+            stack.push(345);
+            Assert.AreEqual(345, stack.peek());
+    }
+
+        //pop() throws EmptyStackException if Stack is empty
+  [TestMethod]
+  [ExpectedException(typeof(EmptyStackException))]
+  public void pop_returns_exception_when_stack_empty()
+   {
+            stack.pop(); 
+   }
+        // pop() returns most recent pushed value
+   [TestMethod]
+   public void pop_returns_most_recent_value()
+   {
+            stack.push(325);
+            Assert.AreEqual(325, stack.pop());
+   }
+        //peek() returns null after pop() is called
+        [TestMethod]
+        public void peek_return_null_after_pop()
+        {
+            stack.push(325);
+            stack.pop();
+            Assert.AreEqual(null,stack.peek());
+        }
+    }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,53 @@
-﻿using System;
+﻿using net_stack.model.exceptions;
+using System;
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Stack
+    {
+        public bool _isEmpty = true;
+
+        public Object obj = null;
+        // Implement code
+        public bool isEmpty()
+        {
+            return _isEmpty;
+        }
+
+        public void push(Object v)
+        {
+            _isEmpty = false;
+            obj = v;
+        }
+
+        public object peek()
+        {
+            if(_isEmpty)
+            {
+                return null;
+            }
+            else
+            {
+                return obj;
+            }
+            
+        }
+        public Object pop()
+        {
+            if (_isEmpty)
+            {
+                throw new EmptyStackException("new exception");
+
+            }
+            else
+            {
+                Object temp = obj;
+                obj = null;
+                return temp;
+
+
+                /*_isEmpty = true;
+                return obj;*/
+            }
+        }
+    }
 }
