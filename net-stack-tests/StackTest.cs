028c361384de9574d1f8021bec5c16202346e4f6
zxanderrodriguez <zxanderrodriguez@gmail.com>
2020-09-17 14:46:38 -0500

    interview 3

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
 
 namespace net_stack_tests
 {
@@ -15,6 +16,84 @@ namespace net_stack_tests
     }
 
     // implement tests
+    [TestMethod]
+    public void IsEmpty()
+        {
+            Assert.IsTrue(stack.IsEmpty());
+        }
 
-  }
+        [TestMethod]
+        public void Is_non_empty_after_push()
+        {
+            stack.Push(new object());
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void Peek_is_null_when_stack_Isempty()
+        {
+            Assert.IsNull(stack.Peek());
+        }
+ 
+        [TestMethod]
+        public void Peek_returns_most_recent_pushed_value()
+        {
+            stack.Push("one");
+            Assert.AreEqual("one", stack.Peek());
+        }
+  
+        [TestMethod]
+        [ExpectedException(typeof(EmptyStackException))]
+        public void Pop_throws_exception_when_stack_empty()
+        {
+            stack.Pop();
+        }
+  
+        [TestMethod]
+        public void Pop_return_most_recent_pushed_value()
+        {
+            stack.Push("one");
+            Assert.AreEqual("one", stack.Pop() as string);
+        }
+        //	come back to this
+        [TestMethod]
+        public void Peek_returns_null_after_Pop()
+        {
+            stack.Push("one");
+            stack.Pop();
+            Assert.IsNull(stack.Peek());
+        }
+        [TestMethod]
+        public void IsEmpty_return_true_after_pop_is_called()
+        {
+            stack.Push("one");
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty());
+        }
+        //	push() twice, pop() returns 2nd pushed value, 
+        //then peek() returns 1st pushed value
+        [TestMethod]
+        public void Pushed_two_elements_read_inReverse_order()
+        {
+            stack.Push("one");
+            stack.Push("two");
+            Assert.AreEqual("two",stack.Pop());
+            Assert.AreEqual("one", stack.Peek());
+        }
+
+        //contains(value) returns true if value is present
+        [TestMethod]
+        public void Returns_true_is_same_value_is_present()
+        {
+            stack.Push("one");
+            Assert.IsTrue(stack.Contains("one"));
+        }
+        //contains(value) returns false if value is not present
+        [TestMethod]
+        public void Returns_false_is_same_value_is_not_present()
+        {
+            stack.Push("one");
+            Assert.IsFalse(stack.Contains("two"));
+        }
+    }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,83 @@
-﻿using System;
+﻿using net_stack.model.exceptions;
+using System;
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Stack
+    {
+
+        private bool _isEmpty = true;
+        private Node _root = null;
+        
+        // Implement code
+        public bool IsEmpty()
+        {
+            return _isEmpty;
+        }
+
+        public void Push(object item)
+        {
+            _isEmpty = false;
+            if (_root == null)
+            {
+                _root = new Node() { data = item, next=null };
+                return;
+            }
+            _root.next = new Node() { data = item, next = null };
+        }
+
+        public object Peek()
+        {
+            if (_isEmpty)
+            {
+                return null;
+            }
+            if (_root.next == null) 
+            {
+                return _root.data;
+            }
+            return _root.next.data;
+        }
+
+        public Object Pop()
+        {
+            if (_isEmpty)
+            {
+                throw new EmptyStackException("new exeption");
+            }
+            Object previuosValue = null;
+            if(_root.next == null)
+            {
+                previuosValue = _root.data;
+                _root = null;
+                _isEmpty = true;
+                return previuosValue;
+            }
+            previuosValue = _root.next.data;
+            _root.next = null;
+            return previuosValue;
+        }
+
+        public bool Contains(Object item)
+        {
+            if (_isEmpty)
+            {
+                return false;
+            }
+            if (_root.data.Equals(item))
+            {
+                return true;
+            }
+            if(_root.next == null)
+            {
+                return false;
+            }
+            return (_root.next.data.Equals(item));
+            
+        }
+    }
+    class Node
+    {
+        public Object data;
+        public Node next;
+    }
 }
