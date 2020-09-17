2a559ae8bcdf75421c78a537f0cb6f274238c3f4
zxanderrodriguez <zxanderrodriguez@gmail.com>
2020-09-17 10:21:29 -0500

    first interview

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,20 +1,88 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
+using System;
 
 namespace net_stack_tests
 {
   [TestClass]
   public class StackTest
   {
-    private Stack stack;
+    private Stack<int?> stack;
 
     [TestInitialize]
     public void SetUp()
     {
-      stack = new Stack();
+      stack = new Stack<int?>();
     }
 
     // implement tests
 
-  }
+    [TestMethod]
+    public void test_isEmpty()
+    {
+       Assert.AreEqual(true, stack.isEmpty());
+    }
+
+    [TestMethod]
+    public void test_AfterPush_isEmpty_false()
+        {
+            stack.push(123);
+            Assert.AreEqual(false, stack.isEmpty());
+        }
+   [TestMethod]
+   public void test_peek_returnsNull_whenStackEmpty()
+        {
+            Assert.IsNull(stack.peek());
+        }
+
+    [TestMethod]
+    public void test_peek_returnLastInsertedValue()
+        {
+            stack.push(123);
+            Assert.AreEqual(123, stack.peek());
+        }
+
+    [TestMethod]
+    [ExpectedException(typeof(EmptyStackException))]
+    public void test_pop_throwsExceptionWhenEmpty()
+        {
+            stack.pop();
+        }
+
+    [TestMethod]
+    public void test_pop_shouldReturnLastValue()
+        {
+            stack.push(123);
+            Assert.AreEqual(123, stack.pop());
+        }
+
+    [TestMethod]
+    public void test_peek_returnNull_afterPop()
+        {
+            stack.push(123);
+            Assert.AreEqual(123, stack.pop());
+            Assert.IsNull(stack.peek());
+        }
+
+    [TestMethod]
+    public void test_isEmpty_returnsTrue_afterPop()
+        {
+            stack.push(123);
+            Assert.AreEqual(123, stack.pop());
+            Assert.IsTrue(stack.isEmpty());
+        }
+    /*push() twice, pop() returns 2nd pushed value, 
+     * then peek() returns 1st pushed value*/
+
+        [TestMethod]
+        public void test_pushTwice_popReturnsSecondValue_peekReturnsFirstPushedValue()
+        {
+            stack.push(1);
+            stack.push(2);
+
+        }
+
+
+    }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,84 @@
-﻿using System;
+﻿using net_stack.model.exceptions;
+using System;
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Stack<T>
+    {
+        private Node<int?> _node = new Node<int?>();
+        private bool _isEmpty = true;
+        // Implement code
+        public bool isEmpty()
+        {
+            return _isEmpty;
+        }
+
+        public void push(int v)
+        {
+          
+            if (this._node.data == null)
+            {
+                this._node.data = v;
+            }
+            else
+            {
+                this._node.next.data = v;
+            }
+
+            //this._val = v;
+            this._isEmpty = false ;
+        }
+
+        public int? peek()
+        {
+
+            if (this._isEmpty)
+            {
+                return null;
+            }
+            else
+            {
+                if(this._node.next.data == null)
+                {
+                    return this._node.data;
+                }
+                else
+                {
+                    return this._node.next.data;
+                }
+            }
+            
+        }
+
+        public int? pop()
+        {
+           
+            
+            if (this._isEmpty)
+            {
+                throw new EmptyStackException("");
+            }
+            else
+            {
+                int? returnVal = null;
+                if (this._node.next.data == null)
+                {
+                    returnVal = this._node.data;
+                }
+                else
+                {
+                    returnVal = this._node.next.data;
+                }
+                this._isEmpty = true;
+                return returnVal;
+                
+            }
+            
+        }
+    }
+    
+    class Node<T>
+    {
+        public T data;
+        public Node<T> next = new Node<T>();
+    }
 }
