d0c23410f674367335e0c1615145eb5d4df3792f
zxanderrodriguez <zxanderrodriguez@gmail.com>
2020-09-18 14:54:31 -0500

    interview 7

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
 
 namespace net_stack_tests
 {
@@ -14,7 +15,85 @@ namespace net_stack_tests
       stack = new Stack();
     }
 
-    // implement tests
+        // implement tests
 
-  }
+        //isEmpty() returns true if Stack is empty
+        [TestMethod]
+        public void CheckStackEmpty()
+        {
+            bool expected = true;
+            Assert.AreEqual(expected,stack.isEmpty());
+        }
+        //	isEmpty() returns false after calling push(value)
+        [TestMethod]
+        public void CheckStackNotEmpty()
+        {
+            stack.push(123);
+            bool expected = false;
+            Assert.AreEqual(expected, stack.isEmpty());
+        }
+        //	peek() returns null if Stack is empty
+        [TestMethod]
+        public void PeekReturnNullWhenEmpty()
+        {
+            Assert.AreEqual(null, stack.peek());
+        }
+        //peek() returns most recent pushed value
+        [TestMethod]
+        public void PeekReturnMostRecentValue()
+        {
+            stack.push(123);
+            Assert.AreEqual(123, stack.peek());
+        }
+        //	pop() throws EmptyStackException if Stack is empty
+        [TestMethod]
+        [ExpectedException(typeof(EmptyStackException))]
+        public void PopException()
+        {
+            stack.pop();
+        }
+        //pop() returns most recent pushed value
+        [TestMethod]
+        public void PopReturnMostRecent()
+        {
+            stack.push(123);
+            Assert.AreEqual(123, stack.pop());
+        }
+        //	peek() returns null after pop() is called
+        [TestMethod]
+        public void PeekReturnsNullAfterPop()
+        {
+            stack.push(123);
+            stack.pop();
+            Assert.AreEqual(null, stack.peek());
+        }
+        //	isEmpty() returns true after pop() is called
+        [TestMethod]
+        public void IsEmptyIsTrueAfterPop()
+        {
+            stack.push(123);
+            stack.pop();
+            Assert.AreEqual(true, stack.isEmpty());
+        }
+        //push() twice, pop() returns 2nd pushed value, 
+        //then peek() returns 1st pushed value
+        [TestMethod]
+        public void PushTwiceReturn()
+        {
+            stack.push(123);
+            stack.push(456);
+            Assert.AreEqual(456, stack.pop());
+            Assert.AreEqual(123, stack.peek());
+        }
+        //	contains(value) returns true if value is present
+        [TestMethod]
+        public void ContainsTrue()
+        {
+            stack.push(123);
+            stack.push(456);
+            stack.push(756);
+            stack.push(952);
+            Assert.AreEqual(true, stack.contains(123));
+        }
+    }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,82 @@
-﻿using System;
+﻿using net_stack.model.exceptions;
+using System;
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Stack
+    {
+        Node head = null;
+        bool _isEmpty = true;
+        // Implement code
+        public bool isEmpty()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void push(Object v)
+        {
+            
+            Node obj = new Node(v);
+            obj.next = head;
+            head = obj;
+
+            _isEmpty = false;
+        }
+        public Object peek()
+        {
+            if(head == null)
+            {
+                return null;
+            }
+            return head.data;
+        }
+
+        public Object pop()
+        {
+            if(head == null)
+            {
+                throw new EmptyStackException("");
+            }
+            else
+            {
+                //head last node == node3
+                // node3 -> node2 -> node1
+                //return node3.data -> back to the user
+                //node2 -> node1 -> null
+                Object obj = head.data;
+                head = head.next;
+                return obj;
+            }
+        }
+        class Node
+        {
+            public Object data;
+            public Node next;
+
+            public Node(Object ent)
+            {
+                this.data = ent;
+                this.next = null;
+            }
+
+        }
+
+        public bool contains(Object v)
+        {
+            Node current = head;
+            while (current.data != null)
+            {
+                if (current.data.Equals(v))
+                {
+                    return true;
+                }
+                current = current.next;
+            }
+            return false;
+            
+        }
+    }
 }
