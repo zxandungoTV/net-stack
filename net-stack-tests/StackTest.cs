c8666ae53006a86d0fbd352f8894cf74e1be0c87
zxanderrodriguez <zxanderrodriguez@gmail.com>
2020-09-18 10:20:51 -0500

    interview 5

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
 
 namespace net_stack_tests
 {
@@ -16,5 +17,83 @@ namespace net_stack_tests
 
     // implement tests
 
-  }
+    [TestMethod]
+    public void isEmpty_true_when_stackIsEmpty()
+        {
+            Assert.AreEqual(true, stack.isEmpty());
+        }
+
+        [TestMethod]
+        public void isEmpty_false_after_calling_push()
+        {
+
+            stack.push("any");
+            Assert.AreEqual(false, stack.isEmpty());
+        }
+
+
+        [TestMethod]
+        public void peek_returns_null_if_stack_isEmpty()
+        {
+            Assert.AreEqual(null, stack.peek());
+        }
+        [TestMethod]
+        public void peek_returns_most_recent()
+        {
+            stack.push("anything");
+            Assert.AreEqual("anything", stack.peek());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyStackException))]
+        public void pop_exception()
+        {
+            stack.pop();
+        }
+
+        [TestMethod]
+        public void pop_returns_most_recent()
+        {
+            stack.push(123);
+            Assert.AreEqual(123,stack.pop());
+        }
+
+        [TestMethod]
+        public void peek_returns_null_after_pop()
+        {
+            stack.push("one");
+            stack.pop();
+            Assert.AreEqual(null, stack.peek());
+        }
+ 
+        [TestMethod]
+        public void isEmpty_returns_true_after_pop()
+        {
+            stack.push("something");
+            stack.pop();
+            Assert.AreEqual(true, stack.isEmpty());
+        }
+
+        //	push() 3 times, pop() 
+        // returns 3rd pushed value, 
+        // pop() returns 2nd pushed value
+        // then peek() returns 1st pushed value
+
+        //push obj1
+        //push ob2 
+        //push obj3
+        //obj3 -> ob2 -> ob1
+
+        [TestMethod]
+        public void push_e_elements_read_them_backwards()
+        {
+            stack.push("one");
+            stack.push("two");
+            stack.push("three");
+            Assert.AreEqual("three", stack.pop());
+            Assert.AreEqual("two", stack.pop());
+            Assert.AreEqual("one", stack.peek());
+
+        }
+    }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,45 @@
-﻿using System;
+﻿using net_stack.model.exceptions;
+using System;
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Stack
+    {
+
+        public Object _value;
+        public bool _isEmpty = true;
+        // Implement code
+        public bool isEmpty()
+        {
+            return _isEmpty;
+        }
+
+        public void push(Object v)
+        {
+            _value = v;
+            _isEmpty = false;
+        }
+
+        public Object peek()
+        {
+            if (isEmpty())
+            {
+                return null;
+            }
+            else
+            {
+                return _value;
+            }
+            
+        }
+
+        public Object pop()
+        {
+            if (_isEmpty)
+            {
+                throw new EmptyStackException("is empty");
+            }
+            _isEmpty = true;
+            return _value;
+        }
+    }
 }
