6e4cada8a9da84748b8922d8bdf502faa8b2a019
zxanderrodriguez <zxanderrodriguez@gmail.com>
2020-09-21 17:02:39 -0500

    interview 11

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,20 +1,126 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
 
 namespace net_stack_tests
 {
-  [TestClass]
-  public class StackTest
-  {
-    private Stack stack;
-
-    [TestInitialize]
-    public void SetUp()
+    [TestClass]
+    public class StackTest
     {
-      stack = new Stack();
+        private Stack stack;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            stack = new Stack();
+        }
+
+        // implement tests
+
+        //	isEmpty() returns true if Stack is empty
+        [TestMethod]
+        public void isEmpty_Test(){
+            bool value = stack.IsEmpty();
+            Assert.IsTrue(value);
+        }
+
+        //isEmpty() returns false after calling push(value)
+        [TestMethod]
+        public void isEmpty_afterPush_test()
+        {
+            stack.Push(4);
+            bool value = stack.IsEmpty();
+
+            Assert.IsFalse(value);
+        }
+
+        //peek() returns null if Stack is empty
+        [TestMethod]
+        public void isEmpty_null_peek_test()
+        {
+            Assert.IsTrue(stack.IsEmpty());
+            Assert.AreEqual(null, stack.Peek());
+        }
+        //	peek() returns most recent pushed value
+        [TestMethod]
+        public void Peek_test()
+        {
+            stack.Push(4);
+            object value = stack.Peek();
+            Assert.AreEqual(4, value);
+        }
+        //pop() throws EmptyStackException if Stack is empty
+        [TestMethod]
+        [ExpectedException(typeof(EmptyStackException))]
+        public void isEmpty_pop_test()
+        {
+            Assert.IsTrue(stack.IsEmpty());
+            stack.Pop();
+
+        }
+        //	pop() returns most recent pushed value
+        [TestMethod]
+        public void Pop_test()
+        {
+            stack.Push(4);
+            object value = stack.Pop();
+            Assert.AreEqual(4, value);
+
+        }
+        //	peek() returns null after pop() is called
+        [TestMethod]
+        public void Null_peek_after_pop()
+        {
+            stack.Push(4);
+            stack.Pop();
+            Assert.AreEqual(null, stack.Peek());
+
+        }
+        //	isEmpty() returns true after pop() is called
+        [TestMethod]
+        public void isEmpty_after_pop()
+        {
+            stack.Push(4);
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty());
+
+        }
+
+        [TestMethod]
+        public void Repeted_integration_test()
+        {
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            Assert.AreEqual(6, stack.Pop());
+            Assert.AreEqual(5, stack.Pop());
+            Assert.AreEqual(4, stack.Peek());
+
+        }
+
+        //	contains(value) returns true if value is present
+
+        [TestMethod]
+        public void contains_test()
+        {
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            Assert.IsTrue(stack.Contains(4));
+
+            Assert.IsTrue(stack.Contains(5));
+
+            Assert.IsTrue(stack.Contains(6));
+
+        }
+        //	contains(value) returns false if value is not present
+        [TestMethod]
+        public void DoesNotContain_test()
+        {
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            Assert.IsFalse(stack.Contains(1));
+        }
     }
-
-    // implement tests
-
-  }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,94 @@
-﻿using System;
+﻿using net_stack.model.exceptions;
+using System;
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Stack
+    {
+        Node top=null;
+        // Implement code
+        public bool IsEmpty()
+        {
+            if(top == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Push(int v)
+        {
+            Node newNode = new Node(v);
+            if(top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+        }
+
+        public object Peek()
+        {
+            if(top == null)
+            {
+                return null;
+            }
+            return top.data;
+        }
+
+        public object Pop()
+        {
+            if (top == null)
+            {
+                throw new EmptyStackException("");
+            }
+            object value = top.data;
+            top = top.next;
+            return value;
+        }
+
+        public bool Contains(int v)
+        {
+            Node value = top;
+            while (value != null)
+            {
+                if (value.data.Equals(v))
+                {
+                    return true;
+                }
+                value = value.next;
+            }
+            return false;
+            /*if (Peek().Equals(v))
+            {
+                return true;
+            }else if(Peek().Equals(null))
+            {
+                return false;
+            }
+            else 
+            {
+                top = top.next;
+                return Contains(v);
+                
+            }*/
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            this.data = d;
+            this.next = null;
+        }
+    }
 }
