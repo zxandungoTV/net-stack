2b652a70c8b1097fbf7212e9fc859efe4e70bec9
zxanderrodriguez <zxanderrodriguez@gmail.com>
2020-09-21 11:52:54 -0500

    interview 9

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+using net_stack.model.exceptions;
 
 namespace net_stack_tests
 {
@@ -14,7 +15,157 @@ namespace net_stack_tests
       stack = new Stack();
     }
 
-    // implement tests
+        // implement tests
 
-  }
+        //isEmpty() returns true if Stack is empty
+        [TestMethod]
+        public void isEmpty_true()
+        {
+            Assert.AreEqual(true,stack.IsEmpty());
+        }
+
+        //	isEmpty() returns false after calling push(value)
+        [TestMethod]
+        public void isEmpty_false()
+        {
+            stack.push(5);
+            Assert.AreEqual(false, stack.IsEmpty());
+        }
+
+        //	peek() returns null if Stack is empty
+        [TestMethod]
+        public void peek_returnsNull()
+        {
+            object data = stack.Peek();
+            Assert.AreEqual(null, data);
+        }
+        //	peek() returns most recent pushed value
+
+        [TestMethod]
+        public void peek_retunsValue()
+        {
+            int test = 3;
+            stack.push(test);
+            object data = stack.Peek();
+            Assert.AreEqual(test, data);
+        }
+        //	pop() throws EmptyStackException if Stack is empty
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyStackException))]
+        public void pop_EmptyException() 
+        {
+            stack.Pop();
+  
+        }
+
+        //	pop() returns most recent pushed value
+        [TestMethod]
+        public void pop_returnValue()
+        {
+            object test = "testDataTest";
+            stack.push(test);
+            object result = stack.Pop();
+            Assert.AreEqual(test, result);
+            
+        }
+
+        //	peek() returns null after pop() is called
+
+        [TestMethod]
+        public void peek_returnNull()
+        {
+
+            object test = "testDataTest";
+            stack.push(test);
+            object pop = stack.Pop();
+            object result = stack.Peek();
+            Assert.AreEqual(null, result);
+        }
+
+        //isEmpty() returns true after pop() is called
+
+        [TestMethod]
+        public void isEmpty_returntrueAfterPop()
+        {
+            object test = "testDataTest";
+            stack.push(test);
+            object pop = stack.Pop();
+            Assert.AreEqual(true, stack.IsEmpty());
+        }
+        //push() twice, pop() returns 2nd pushed value, then peek() returns 1st pushed value
+        [TestMethod]
+        public void push_Pop_peek_test()
+        {
+            object test_1 = "testDataOne";
+            stack.push(test_1);
+            object test_2 = "testDataTwo";
+            stack.push(test_2);
+            object test_3 = "testDataThree";
+            stack.push(test_3);
+
+            object pop = stack.Pop();
+            Assert.AreEqual(test_3, pop);
+
+            pop = stack.Pop();
+            Assert.AreEqual(test_2, pop);
+
+            pop = stack.Peek();
+            Assert.AreEqual(test_1, pop);
+
+        }
+
+        //	contains(value) returns true if value is present
+        [TestMethod]
+        public void contains_test()
+        {
+            object test_1 = "testDataOne";
+            stack.push(test_1);
+            object test_2 = "testDataTwo";
+            stack.push(test_2);
+            object test_3 = "testDataThree";
+            stack.push(test_3);
+
+            object contains = "testDataTwo";
+
+            Assert.AreEqual(true, stack.Contains(contains));
+            Assert.AreEqual(true, stack.Contains(test_1));
+            Assert.AreEqual(true, stack.Contains(test_3));
+
+        }
+
+        //contains(value) returns false if value is not present
+        [TestMethod]
+        public void contains_test_false()
+        {
+            object test_1 = "testDataOne";
+            stack.push(test_1);
+            object test_2 = "testDataTwo";
+            stack.push(test_2);
+            object test_3 = "testDataThree";
+            stack.push(test_3);
+
+            object contains = "data";
+
+            Assert.AreEqual(false, stack.Contains(contains));
+            Assert.AreEqual(false, stack.Contains("dfasdf"));
+            Assert.AreEqual(false, stack.Contains("dfsdf"));
+
+        }
+
+        //size() returns number of elements in Stack (tracked by variable)
+       /* [TestMethod]
+        public void size_Test()
+        {
+            object test_1 = "testDataOne";
+            stack.push(test_1);
+            object test_2 = "testDataTwo";
+            stack.push(test_2);
+            object test_3 = "testDataThree";
+            stack.push(test_3);
+
+            Assert.AreEqual(3, stack.Size());
+
+        }*/
+    }
 }
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,8 +1,100 @@
-﻿using System;
+﻿using net_stack.model.exceptions;
+using System;
 namespace net_stack
 {
-  public class Stack
-  {
-    // Implement code
-  }
+    public class Stack
+    {
+        Node top = null;
+        public int size = 0;
+        // Implement code
+        public bool IsEmpty()
+        {
+            if(top == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void push(Object v)
+        {
+            Node newNode = new Node(v);
+
+            if(top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+        }
+
+        public object Peek()
+        {
+            if(top == null)
+            {
+                return null;
+            }
+            else
+            {
+                return top.data;
+
+            }
+        }
+
+        public Object Pop()
+        {
+            Node temp = top;
+            if (IsEmpty())
+            {
+                throw new EmptyStackException("");
+
+            }
+            else
+            {
+                top = top.next;
+                return temp.data;
+            }
+        }
+
+        public bool Contains(object contains)
+        {
+            Node current = top;
+            
+            while (current.next != null)
+            {
+                if (current.data.Equals(contains))
+                {
+                    return true;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+            return current.data.Equals(contains);
+        }
+
+        public int Size()
+        {
+            return this.size;
+        }
+    }
+
+    class Node
+    {
+        public Object data;
+        public Node next;
+
+        public Node (Object data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
